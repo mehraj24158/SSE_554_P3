35819365435d56a9e74e8b7bc73c2a551632e659
Mehraj_Ahmed <mehraj24158@gmail.com>
2021-03-17 20:06:29 -0400

    completed stacks and queues

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -5,6 +5,7 @@
 #include "src/lib/frame.hpp"
 #include "src/lib/tire.hpp"
 #include "src/lib/operations.hpp"
+#include "stack"
 
 // // For example vector of 5 cars randomly generated
 // // loop through each car and a seperate vector containing each thread will install the neccessary part
@@ -83,3 +84,73 @@ TEST(EmptyCar, VectorCompleteInstall){
         ASSERT_TRUE(P->tire.size()==4) << P->tire.size() << " Number of tires";
     }
 };
+
+// Fixture, contains 100 default cars
+// use Test_F(Inventory, Test-name) to use
+class Inventory: public ::testing::Test 
+{
+    protected:
+        std::vector<Car*> cars;
+
+        void SetUp()
+        {
+            for(int i = 0; i<100; i++)
+            {
+                Car one;
+                Car* P = &one;
+                cars.push_back(P);
+            }
+
+            std::thread engine(EngineInstallerVect, std::ref(cars));
+            std::thread frame(FrameInstallerVect, std::ref(cars));
+            std::thread tire(TireInstallerVect, std::ref(cars));
+
+            engine.join();
+            frame.join();
+            tire.join();
+        }    
+};
+
+// Recall new cars with a stack
+TEST_F(Inventory, Recall)
+{
+    std::stack<Car*> car_stack;
+
+    // Manufactured car records are stored in stack
+    for(Car* i : cars)
+    {
+        car_stack.push(i);
+    }
+
+    std::stack<Car*> recall_list;
+
+    // Recall last 20 cars
+    for(int i = 0; i<20; i++)
+    {
+        recall_list.push(car_stack.top());
+        car_stack.pop();
+    }
+};
+
+//Ship cars off the assembly line with a shipping queue
+TEST_F(Inventory, Ship)
+{
+    std::stack<Car*> car_queue;
+
+    // Manufactured car records are stored in stack
+    for(Car* i : cars)
+    {
+        car_queue.push(i);
+    }
+
+    std::stack<Car*> ship_list;
+
+    for(int i = 0; i<20; i++)
+    {
+        ship_list.push(car_queue.top());
+        car_queue.pop();
+    }
+};
+
+//Sort Cars by their price
+// TEST_F(Inventory, Heap)
