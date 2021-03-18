a5c4d46e3c347b7aaa6a53dd21e145492fcfb9ae
Usman Khan <khan.usmanr@gmail.com>
2021-03-17 22:42:25 -0400

    linearSearch

---
+++ src/lib/linearSearch.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "linearSearch.hpp"
+#include "car.hpp"
+using namespace std; 
+
+
+Car 
+int search(int arr[], int n, int car.id)
+{  
+    for (int i = 0; i<n; i++)
+    {
+        if(arr[i] == car.id)
+            return i; 
+    }
+    return -1; 
+}
+
